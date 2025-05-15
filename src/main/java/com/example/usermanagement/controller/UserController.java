06ea85bb2470fe61b284500ad235a4b739278fee
mohammadhoseinbaba <sirhb77@gmail.com>
2025-05-15 18:25:57 +0200

    added Usercontroller

---
+++ src/main/java/com/example/usermanagement/controller/UserController.java
@@ -0,0 +1,28 @@
+package com.example.usermanagement.controller;
+
+import com.example.usermanagement.model.User;
+import com.example.usermanagement.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+    @Autowired
+    private UserService userService;
+
+    @PostMapping
+    public User createUser(@RequestBody User user) {
+        return userService.createUser(user);
+    }
+
+    @PostMapping
+    public List<User> getAllUsers() {
+        return userService.getAllUsers();
+    }
+}
