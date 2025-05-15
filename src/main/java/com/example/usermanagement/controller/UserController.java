cf438496d7fa41f10ab32e80a8294e8bc2fbaa34
mohammadhoseinbaba <sirhb77@gmail.com>
2025-05-15 18:33:18 +0200

    updated

--- src/main/java/com/example/usermanagement/controller/UserController.java
+++ src/main/java/com/example/usermanagement/controller/UserController.java
@@ -3,12 +3,10 @@ package com.example.usermanagement.controller;
 import com.example.usermanagement.model.User;
 import com.example.usermanagement.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/users")
@@ -21,8 +19,24 @@ public class UserController {
         return userService.createUser(user);
     }
 
-    @PostMapping
+    @GetMapping
     public List<User> getAllUsers() {
         return userService.getAllUsers();
     }
+
+    @GetMapping({"/{id}"})
+    public Optional<User> getUserById(@PathVariable Long id) {
+        return userService.getUserById(id);
+    }
+
+    @PutMapping("{id}")
+    public User updateUser(@PathVariable Long id, @RequestBody User user) {
+        return userService.updateUser(id, user);
+    }
+
+    @DeleteMapping("{id}")
+    public void deleteUser(@PathVariable Long id) {
+        userService.deleteUser(id);
+    }
+
 }
